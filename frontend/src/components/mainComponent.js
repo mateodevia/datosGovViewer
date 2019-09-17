6f3664c1fa014ad4ec904453af0f3d88086269b9
mateo <m.devia@uniandes.edu.co>
2019-09-17 17:19:20 -0500

    bono y paginas (componente creativo)

--- frontend/src/components/mainComponent.js
+++ frontend/src/components/mainComponent.js
@@ -5,37 +5,55 @@ class MainComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      url: "https://www.datos.gov.co/resource/hiku-z4v3.json",
+      url: "",
       data: [],
-      cargo: false
+      pidioPaginas: false,
+      pagina: 1
     };
   }
 
   handleClick = () => {
-    this.state.cargo = true;
     const promesa1 = fetch(this.state.url,
       {
         headers: {
           "Content-Type": "application/json"
         }
       });
-
     const promesa2 = promesa1.then((res) => {
       return res.json();
     });
-
     promesa1.catch((err) => {
-      alert(err);
+      alert("Lo siento la url no es valida");
     });
-
     promesa2.then((res) => {
-      console.log(res);
-
-      this.setState({ data: res });
+      this.setState({ data: res, pidioPaginas: true });
     });
 
     promesa2.catch((err) => {
-      alert(err);
+      alert("Lo siento la url no es valida");
+    });
+  }
+
+  handleClick2 = () => {
+
+    const promesa1 = fetch(this.state.url + "?$select=count(*)",
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+    const promesa2 = promesa1.then((res) => {
+      return res.json();
+    });
+    promesa1.catch((err) => {
+      alert("Lo siento la url no es valida");
+    });
+    promesa2.then((res) => {
+      this.pedirTodos(res[0].count);
+    });
+
+    promesa2.catch((err) => {
+      alert("Lo siento la url no es valida");
     });
   }
 
@@ -45,17 +63,100 @@ class MainComponent extends Component {
     });
   }
 
+  pedirPagina = (numPagina) => {
+
+    console.log("NUM PAGINA", numPagina);
+
+
+    let offSet = 1000 * numPagina;
+
+    let request = this.state.url + "?$offset=" + offSet
+
+    console.log(request);
+
+
+    const promesa1 = fetch(request,
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+    const promesa2 = promesa1.then((res) => {
+      return res.json();
+    });
+    promesa1.catch((err) => {
+      alert("Lo siento la url no es valida");
+    });
+    promesa2.then((res) => {
+      this.setState({ data: res });
+    });
+
+    promesa2.catch((err) => {
+      alert("Lo siento la url no es valida");
+    });
+  }
+
+  pedirTodos = (numeroRegistros) => {
+
+    let request = this.state.url + "?$limit=" + numeroRegistros;
+
+    const promesa1 = fetch(request,
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+    const promesa2 = promesa1.then((res) => {
+      return res.json();
+    });
+    promesa1.catch((err) => {
+      alert("Lo siento la url no es valida");
+    });
+    promesa2.then((res) => {
+      this.setState({ data: res });
+    });
+
+    promesa2.catch((err) => {
+      alert("Lo siento la url no es valida");
+    });
+  }
+
+  siguiente = () => {
+
+    let nuevaPagina = this.state.pagina + 1
+
+    this.setState({ pagina: nuevaPagina })
+    this.pedirPagina(nuevaPagina - 1)
+  }
+
+  anterior = () => {
+
+    let nuevaPagina = this.state.pagina - 1
+
+    this.setState({ pagina: nuevaPagina })
+    this.pedirPagina(nuevaPagina - 1)
+  }
+
   render() {
     return (
       <React.Fragment>
+        <h1>Bienvenido a Datos Gov View</h1>
+        <h3>Por favor ingresa el link del data set que deseas visualizar y nosotros haremos el resto</h3>
         <input ref={myIn => this.myIn = myIn} type="text" placeholder="url" value={this.state.url} onChange={this.handleChange.bind(this)} />
         <button onClick={this.handleClick}>Traer Datos</button>
-        {this.state.cargo &&
-          <Navio data={this.state.data}></Navio>
+        <button onClick={this.handleClick2}>Traer Todos los Datos</button>
+        <p>NOTA: si deseas que la consulta sea mas rapida oprime el boton de traer datos. Esto te mostrara los primeros 1000 datos. Si queres visualizar todo, oprime Traer Todos los dtaos</p>
+        <Navio data={this.state.data}></Navio>
+        {this.state.pidioPaginas &&
+          <React.Fragment>
+            <h1>Pagina</h1>
+            <h3>{this.state.pagina}</h3>
+            <button onClick={this.anterior}>Anterior</button>
+            <button onClick={this.siguiente}>Siguiente</button>
+          </React.Fragment>
         }
       </React.Fragment>
     );
   }
 }
-
 export default MainComponent;
--- frontend/src/components/navio.js
+++ frontend/src/components/navio.js
@@ -7,11 +7,42 @@ class Navio extends Component {
     this.state = {}
   }
 
-  componentDidMount() {
+  requestData = () => {
+    this.state.cargo = true;
+    const promesa1 = fetch(this.state.url,
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+
+    const promesa2 = promesa1.then((res) => {
+      return res.json();
+    });
+
+    promesa1.catch((err) => {
+      alert(err);
+    });
+
+    promesa2.then((res) => {
+      console.log(res);
+
+      this.setState({ data: res });
+    });
+
+    promesa2.catch((err) => {
+      alert(err);
+    });
+  }
+
+  componentDidUpdate() {
     console.log("DATA", this.props.data);
     let nv = navio(this.myDiv, 600);
-    nv.data(this.props.data);
-    nv.addAllAttribs();
+
+    if (this.props.data.length) {
+      nv.data(this.props.data);
+      nv.addAllAttribs();
+    }
   }
 
   render() {
